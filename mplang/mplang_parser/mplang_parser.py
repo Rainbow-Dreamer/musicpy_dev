5f3a17fbdf7ee62c75db6a561d90cf80ed0d25ec
Rainbow <2180502841@qq.com>
2021-11-17 02:54:19 +0800

    Update mplang_parser.py

--- mplang/mplang_parser/mplang_parser.py
+++ mplang/mplang_parser/mplang_parser.py
@@ -1,10 +1,11 @@
 from musicpy import *
+import traceback
 
 piece_attributes = [
     'tracks', 'instruments_list', 'bpm', 'start_times', 'track_names',
     'channels', 'name', 'pan', 'volume', 'other_messages', 'sampler_channels'
 ]
-piece_attributes_str = ['name']
+piece_attributes_str = ['name', 'track_names']
 track_attributes = [
     'content', 'instrument', 'start_time', 'channel', 'track_name', 'pan',
     'volume', 'bpm', 'name', 'sampler_channel'
@@ -12,6 +13,10 @@ track_attributes = [
 track_attributes_str = ['track_name', 'name']
 scale_attributes_str = ['start', 'mode', 'name']
 sampler_attributes = ['name', 'num', 'bpm']
+define_state = False
+define_variable = None
+define_body = []
+python_state = False
 
 
 def parser(text=None, file=None):
@@ -38,7 +43,7 @@ def parser(text=None, file=None):
     return '\n'.join(lines)
 
 
-def make_token_parser(lines, i, remain_part):
+def make_token_parser(lines=None, i=None, remain_part=None, current=None):
     if '=' in remain_part:
         variable_name, definition_part = remain_part.split('=', 1)
     else:
@@ -47,49 +52,97 @@ def make_token_parser(lines, i, remain_part):
     definition_part = definition_part.lstrip()
     current_definition_split = definition_part.split(' ', 1)
     if len(current_definition_split) == 1:
-        lines[i] = remain_part
+        if current is not None:
+            return remain_part
+        else:
+            lines[i] = remain_part
     elif len(current_definition_split) == 2:
         data_type_token, current_data = current_definition_split
         current_data = current_data.strip()
         if data_type_token == 'chord':
-            make_chord_parser(lines, variable_name, current_data, i)
+            result = make_chord_parser(lines, variable_name, current_data, i,
+                                       current)
         elif data_type_token == 'note':
-            make_note_parser(lines, variable_name, current_data, i)
+            result = make_note_parser(lines, variable_name, current_data, i,
+                                      current)
         elif data_type_token == 'scale':
-            make_scale_parser(lines, variable_name, current_data, i)
+            result = make_scale_parser(lines, variable_name, current_data, i,
+                                       current)
         elif data_type_token == 'piece':
-            make_piece_parser(lines, variable_name, current_data, i)
+            result = make_piece_parser(lines, variable_name, current_data, i,
+                                       current)
         elif data_type_token == 'drum':
-            make_drum_parser(lines, variable_name, current_data, i)
+            result = make_drum_parser(lines, variable_name, current_data, i,
+                                      current)
         else:
-            lines[i] = remain_part
+            if current is not None:
+                return remain_part
+            else:
+                lines[i] = remain_part
+        if current is not None:
+            return result
 
 
-def define_token_parser(lines, i, remain_part):
+def define_token_parser(lines=None, i=None, remain_part=None, current=None):
+    global define_state
+    global define_variable
     type_name, variable_name = remain_part.split(' ', 1)
     if type_name == 'piece':
-        define_piece_parser(lines, i, variable_name)
+        if current:
+            define_state = 'piece'
+            define_variable = variable_name
+        else:
+            define_piece_parser(lines, i, variable_name)
     elif type_name == 'drum':
-        define_drum_parser(lines, i, variable_name)
+        if current:
+            define_state = 'drum'
+            define_variable = variable_name
+        else:
+            define_drum_parser(lines, i, variable_name)
     elif type_name == 'sampler':
-        define_sampler_parser(lines, i, variable_name)
+        if current:
+            define_state = 'sampler'
+            define_variable = variable_name
+        else:
+            define_sampler_parser(lines, i, variable_name)
+    if current:
+        return ''
 
 
-def single_token_parser(lines, i, current_split):
+def single_token_parser(lines=None, i=None, current_split=None, current=None):
     if current_split[0] == 'python:':
-        python_code_parser(lines, i)
+        if not current:
+            python_code_parser(lines, i)
+        else:
+            global python_state
+            python_state = True
+            return ''
+    else:
+        if current:
+            return current
 
 
-def special_token_parser(start_token, lines, i, remain_part):
+def special_token_parser(start_token,
+                         lines=None,
+                         i=None,
+                         remain_part=None,
+                         current=None):
     if start_token != '#':
         if start_token == 'use':
-            lines[i] = f'from {remain_part} import *'
+            result = f'from {remain_part} import *'
         elif start_token == 'import':
-            lines[i] = f'import {remain_part}'
+            result = f'import {remain_part}'
         elif start_token == 'python':
-            lines[i] = remain_part
+            result = remain_part
         else:
-            lines[i] = f'{start_token}({remain_part})'
+            result = f'{start_token}({remain_part})'
+        if current:
+            return result
+        else:
+            lines[i] = result
+    else:
+        if current:
+            return current
 
 
 def python_code_parser(lines, i):
@@ -102,18 +155,28 @@ def python_code_parser(lines, i):
     del lines[i + 1:ind + 1]
 
 
-def make_chord_parser(lines, variable_name, current_data, i):
+def make_chord_parser(lines=None,
+                      variable_name=None,
+                      current_data=None,
+                      i=None,
+                      current=None):
     if current_data.startswith('(') and ')' in current_data:
         right_bracket_ind = current_data.index(')')
-        lines[
-            i] = f'{variable_name} = chord("{current_data[1:right_bracket_ind]}"){"".join(current_data[right_bracket_ind+1:])}'
+        result = f'{variable_name} = chord("{current_data[1:right_bracket_ind]}"){"".join(current_data[right_bracket_ind+1:])}'
     else:
         current_data_split = current_data.split(' ', 1)
-        lines[
-            i] = f'{variable_name} = C("{current_data_split[0]}"){"".join(current_data_split[1:])}'
+        result = f'{variable_name} = C("{current_data_split[0]}"){"".join(current_data_split[1:])}'
+    if current:
+        return result
+    else:
+        lines[i] = result
 
 
-def make_note_parser(lines, variable_name, current_data, i):
+def make_note_parser(lines=None,
+                     variable_name=None,
+                     current_data=None,
+                     i=None,
+                     current=None):
     current_data_split = current_data.split(' ', 1)
     current_data_config_part = "".join(current_data_split[1:]).strip()
     if current_data_config_part.startswith(
@@ -121,14 +184,20 @@ def make_note_parser(lines, variable_name, current_data, i):
         current_data_config_part = ','.join([
             '='.join(r.split(' ')) for r in current_data_config_part.split(',')
         ])
-        lines[
-            i] = f'{variable_name} = N("{current_data_split[0]}").reset{current_data_config_part}'
+        result = f'{variable_name} = N("{current_data_split[0]}").reset{current_data_config_part}'
     else:
-        lines[
-            i] = f'{variable_name} = N("{current_data_split[0]}"){current_data_config_part}'
+        result = f'{variable_name} = N("{current_data_split[0]}"){current_data_config_part}'
+    if current:
+        return result
+    else:
+        lines[i] = result
 
 
-def make_scale_parser(lines, variable_name, current_data, i):
+def make_scale_parser(lines=None,
+                      variable_name=None,
+                      current_data=None,
+                      i=None,
+                      current=None):
     if not (current_data.startswith('(') and current_data.endswith(')')):
         raise SyntaxError(
             'using `let` to construct a scale instance must start with `(` and end with `)`, for example `scale (C5 major)`'
@@ -156,10 +225,18 @@ def make_scale_parser(lines, variable_name, current_data, i):
         '='.join(each_scale_attribute)
         for each_scale_attribute in current_scale_dict.items()
     ])
-    lines[i] = f'{variable_name} = scale({current_data_config_part})'
+    result = f'{variable_name} = scale({current_data_config_part})'
+    if current:
+        return result
+    else:
+        lines[i] = result
 
 
-def make_piece_parser(lines, variable_name, current_data, i):
+def make_piece_parser(lines=None,
+                      variable_name=None,
+                      current_data=None,
+                      i=None,
+                      current=None):
     if not (current_data.startswith('{') and current_data.endswith('}')):
         raise SyntaxError(
             'using `let` to construct a piece instance must start with `{` and end with `}`, for example `piece {tracks: (c1, c2)}`'
@@ -203,15 +280,25 @@ def make_piece_parser(lines, variable_name, current_data, i):
         result_attributes = ",".join(
             ["=".join(each) for each in current_piece_dict.items()])
         result = f'piece({result_attributes})'
-    lines[i] = result
+    if current:
+        return result
+    else:
+        lines[i] = result
 
 
-def make_drum_parser(lines, variable_name, current_data, i):
+def make_drum_parser(lines=None,
+                     variable_name=None,
+                     current_data=None,
+                     i=None,
+                     current=None):
     result = f'{variable_name} = drum("{current_data}")'
-    lines[i] = result
+    if current:
+        return result
+    else:
+        lines[i] = result
 
 
-def define_piece_parser(lines, i, variable_name):
+def define_piece_parser(lines, i, variable_name, current=None):
     j = i
     current_definition_range = i, len(lines)
     while j < len(lines):
@@ -235,17 +322,25 @@ def define_piece_parser(lines, i, variable_name):
             elif attribute_token == 'bpm':
                 attribute_value = attribute_value.strip()
                 piece_attribute_dict['bpm'] = attribute_value
-            elif attribute_token == 'tracks':
-                piece_attribute_dict['tracks'] = f'[{attribute_value}]'
             elif attribute_token == 'track':
                 track_parser(lines, current_definition_range, piece_tracks,
                              each)
+            else:
+                if attribute_token in piece_attributes_str:
+                    current_text = ",".join(
+                        [f'"{t.strip()}"' for t in attribute_value.split(",")])
+                    piece_attribute_dict[attribute_token] = f'[{current_text}]'
+                else:
+                    piece_attribute_dict[
+                        attribute_token] = f'[{attribute_value}]'
     if piece_tracks:
         current_piece_text = f'build({",".join(piece_tracks)},{",".join([f"{each_piece_attribute}={piece_attribute_dict[each_piece_attribute]}" for each_piece_attribute in piece_attribute_dict])})'
     else:
         current_piece_text = f'piece({",".join([f"{each_piece_attribute}={piece_attribute_dict[each_piece_attribute]}" for each_piece_attribute in piece_attribute_dict])})'
-    lines[current_definition_range[
-        0]] = f'{variable_name} = {current_piece_text}'
+    result = f'{variable_name} = {current_piece_text}'
+    if current:
+        return result
+    lines[current_definition_range[0]] = result
     del lines[current_definition_range[0] + 1:current_definition_range[1] + 1]
 
 
@@ -262,6 +357,7 @@ def track_parser(lines, current_definition_range, piece_tracks, each):
                 each_track_split_info = each_track_split.split(' ', 1)
                 if len(each_track_split_info) == 2:
                     each_track_split_attribute, each_track_split_value = each_track_split_info
+                    each_track_split_value = each_track_split_value.strip()
                     if each_track_split_attribute in track_attributes_str:
                         each_track_split_value = f'"{each_track_split_value}"'
                     elif each_track_split_attribute == 'instrument' and not each_track_split_value.strip(
@@ -313,7 +409,7 @@ def sampler_channel_parser(lines, current_definition_range, each,
             break
 
 
-def define_drum_parser(lines, i, variable_name):
+def define_drum_parser(lines, i, variable_name, current=None):
     j = i
     current_definition_range = i, len(lines)
     while j < len(lines):
@@ -339,12 +435,14 @@ def define_drum_parser(lines, i, variable_name):
             else:
                 drum_attribute_dict[attribute_token] = attribute_value
     current_drum_text = f'drum({",".join([f"{each_drum_attribute}={drum_attribute_dict[each_drum_attribute]}" for each_drum_attribute in drum_attribute_dict])})'
-    lines[
-        current_definition_range[0]] = f'{variable_name} = {current_drum_text}'
+    result = f'{variable_name} = {current_drum_text}'
+    if current:
+        return result
+    lines[current_definition_range[0]] = result
     del lines[current_definition_range[0] + 1:current_definition_range[1] + 1]
 
 
-def define_sampler_parser(lines, i, variable_name):
+def define_sampler_parser(lines, i, variable_name, current=None):
     j = i
     current_definition_range = i, len(lines)
     while j < len(lines):
@@ -385,11 +483,67 @@ def define_sampler_parser(lines, i, variable_name):
                     current_channels_text += f'{variable_name}.load({current_channel_num}, {current_channel_sound})\n'
         current_channels_text = current_channels_text[:-1]
         current_sampler_text += current_channels_text
-    lines[current_definition_range[
-        0]] = f'{variable_name} = {current_sampler_text}'
+    result = f'{variable_name} = {current_sampler_text}'
+    if current:
+        return result
+    lines[current_definition_range[0]] = result
     del lines[current_definition_range[0] + 1:current_definition_range[1] + 1]
 
 
 def parse(text=None, file=None):
     result = parser(text, file)
     exec(result, globals(), globals())
+
+
+def interactive_parse():
+    global python_state
+    global define_state
+    global define_variable
+    print('Welcome to mplang')
+    while True:
+        current = input()
+        if current.strip():
+            try:
+                if python_state:
+                    if current.strip() == 'end':
+                        python_state = False
+                        current = ''
+                elif define_state:
+                    if current.strip() == 'end':
+                        if define_state == 'piece':
+                            current = define_piece_parser(
+                                define_body, 0, define_variable, current)
+                        elif define_state == 'drum':
+                            current = define_drum_parser(
+                                define_body, 0, define_variable, current)
+                        elif define_state == 'sampler':
+                            current = define_sampler_parser(
+                                define_body, 0, define_variable, current)
+                        define_state = False
+                        define_variable = None
+                        define_body.clear()
+                    else:
+                        define_body.append(current)
+                        current = ''
+                else:
+                    current_split = current.split(' ', 1)
+                    current_split_length = len(current_split)
+                    if current_split_length == 2:
+                        start_token, remain_part = current_split
+                        if start_token == 'let':
+                            current = make_token_parser(
+                                remain_part=remain_part, current=current)
+                        elif start_token == 'define':
+                            current = define_token_parser(
+                                remain_part=remain_part, current=current)
+                        else:
+                            current = special_token_parser(
+                                start_token,
+                                remain_part=remain_part,
+                                current=current)
+                    elif current_split_length == 1:
+                        current = single_token_parser(
+                            current_split=current_split, current=current)
+                exec(current, globals(), globals())
+            except Exception as e:
+                print(traceback.format_exc())
