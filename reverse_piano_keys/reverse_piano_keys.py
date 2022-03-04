091246584b72fd9ff12219baeaa7c95d76820428
Rainbow-Dreamer <2180502841@qq.com>
2022-03-05 01:14:37 +0800

    Update reverse_piano_keys.py

--- reverse_piano_keys/reverse_piano_keys.py
+++ reverse_piano_keys/reverse_piano_keys.py
@@ -7,25 +7,25 @@ but the piano keys are reversed (the lowest key become the highest key, and so o
 just enjoy this little fun function~
 
 Usage:
-# firstly read a MIDI file as a piece instance,
-# and then apply merge function to it in order to get a chord instance
-bpm, test_piece, start_time = mp.read(file_path).merge()
+# firstly read a MIDI file as a piece instance
+test_piece = mp.read(file_path)
 
-# convert to a new chord instance with reversed piano keys applied
+# convert to a new piece instance with reversed piano keys applied
 test_piece_new = reverse_piano_keys(test_piece)
 
 # play the converted piece and enjoy
-mp.play(test_piece_new, bpm=bpm, start_time=start_time)
+mp.play(test_piece_new)
 '''
 
 
 def reverse_piano_keys(obj):
     temp = mp.copy(obj)
-    for i, each in enumerate(temp.notes):
-        if type(each) == mp.note:
-            reverse_note = mp.degree_to_note(87 - (each.degree - 21) + 21)
-            reverse_note.channel = each.channel
-            reverse_note.duration = each.duration
-            reverse_note.volume = each.volume
-            temp.notes[i] = reverse_note
+    for k in temp.tracks:
+        for i, each in enumerate(k.notes):
+            if type(each) == mp.note:
+                reverse_note = mp.degree_to_note(87 - (each.degree - 21) + 21)
+                reverse_note.channel = each.channel
+                reverse_note.duration = each.duration
+                reverse_note.volume = each.volume
+                k.notes[i] = reverse_note
     return temp
