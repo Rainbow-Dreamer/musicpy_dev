c5678cbfcdfa5ed0864c70236f8e681ab44bc1fa
Rainbow-Dreamer <2180502841@qq.com>
2022-10-07 03:11:48 +0800

    update

---
+++ mplang/mplang_parser/mplang start program.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import traceback
+import chunk
+import midiutil
+import mido
+import fractions
+import pygame
+from pydub import AudioSegment
+from pydub.generators import Sine, Triangle, Sawtooth, Square, WhiteNoise, Pulse
+import py
+import pygame.midi
+import time
+
+abs_path = os.path.dirname(sys.executable)
+os.chdir(abs_path)
+sys.path.append('.')
+
+from musicpy import *
+
+with open('mplang.py', encoding='utf-8') as f:
+    exec(f.read())
